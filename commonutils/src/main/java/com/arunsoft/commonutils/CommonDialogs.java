6b9b5dbe03108ab6503cd7457610a7c8a79b5c17
S Praveen Kumar <35139208+s-praveen-kumar@users.noreply.github.com>
2018-01-08 16:10:22 +0530

    Add text input dialog

--- commonutils/src/main/java/com/arunsoft/commonutils/CommonDialogs.java
+++ commonutils/src/main/java/com/arunsoft/commonutils/CommonDialogs.java
@@ -10,17 +10,18 @@ import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.widget.EditText;
 
 public class CommonDialogs {
 
     public static void InfoDialog(@NonNull Context c, String title, String msg, @Nullable String closeText) {
         if (closeText == null)
             closeText = "Close";
-        new AlertDialog.Builder(c).setMessage(msg).setTitle(title).setPositiveButton(closeText, null).create().show();
+        createDialog(c, title, msg).setPositiveButton(closeText, null).create().show();
     }
 
     public static void ConfirmationDialog(@NonNull Context c, String title, String msg, String yesButtonText, String noButtonText, final ResponseListener listener) {
-        new AlertDialog.Builder(c).setTitle(title).setMessage(msg).setPositiveButton(yesButtonText, new DialogInterface.OnClickListener() {
+        createDialog(c, title, msg).setPositiveButton(yesButtonText, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 listener.onResponse(true);
@@ -30,14 +31,40 @@ public class CommonDialogs {
             public void onClick(DialogInterface dialogInterface, int i) {
                 listener.onResponse(false);
             }
-        }).create().show();
+        }).setCancelable(false).create().show();
     }
 
     public static void ConfirmationDialog(@NonNull Context c, String title, String msg, final ResponseListener listener) {
         ConfirmationDialog(c, title, msg, "Yes", "No", listener);
     }
 
+    public static void InputTextDialog(@NonNull Context c, String title, String msg, boolean multiLine, final TextEnteredListener listener) {
+        final EditText editText = new EditText(c);
+        editText.setSingleLine(!multiLine);
+        createDialog(c, title, msg).setView(editText).setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                listener.onTextEntered(editText.getText().toString());
+            }
+        }).setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                listener.onCanceled();
+            }
+        }).create().show();
+    }
+
     public abstract static class ResponseListener {
         abstract void onResponse(boolean response);
+    }
+
+    public abstract static class TextEnteredListener {
+        abstract void onTextEntered(String text);
+
+        abstract void onCanceled();
+    }
+
+    private static AlertDialog.Builder createDialog(@NonNull Context c, String title, String msg) {
+        return new AlertDialog.Builder(c).setMessage(msg).setTitle(title);
     }
 }
